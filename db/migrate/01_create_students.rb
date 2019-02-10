2a7ef612b5d3647e54618eb6000d057cd7f06598
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:23:11 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,10 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :student do |t|
       t.string :name
-      t.string :dance
-      t.integer :age
-      t.string :hometown
-      t.string :favorite_food
     end
   end
 end
