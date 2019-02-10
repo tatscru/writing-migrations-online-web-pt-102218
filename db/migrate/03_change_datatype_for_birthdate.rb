798ff3d5e0c06c22a3334e2a4bd06574381e1cef
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:22:12 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column(:students, :birthdate, :datetime )
+    change_column(:student, :birthdate, :datetime )
     end 
   end 
